cb8f33d48c47825aa01b4624e9f37c2441d86b69
Noel Power <noel.power@suse.com>
2025-03-28 12:00:39 +0000

    python/tests: add simple smb named pipe echo test
    This will be a regression test for the npsd_echo_server
    coming in the next commits.
    
    Pair-Programmed-With: Stefan Metzmacher <metze@samba.org>
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

---
+++ python/samba/tests/nps_echo_test.py
@@ -0,0 +1,50 @@
+# Unix SMB/CIFS implementation.
+#
+# # Copyright (C) Noel Power <npower@samba.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from samba.samba3 import libsmb_samba_internal as libsmb
+from samba.samba3 import param as s3param
+import samba.tests
+import os
+from samba.credentials import Credentials
+
+class TestNPSEcho(samba.tests.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.lp = s3param.get_context()
+        self.server_ip = os.environ["SERVER_IP"]
+        self.user = os.environ["USER"]
+        self.passwd = os.environ["PASSWORD"]
+        self.creds = Credentials()
+        self.creds.guess(self.lp)
+        self.creds.set_username(self.user)
+        self.creds.set_password(self.passwd)
+
+    def test_msg_nps_echo(self):
+        c = libsmb.Conn(
+            self.server_ip,
+            "ipc$",
+            self.lp,
+            self.creds)
+
+        fnum = c.create("nps_echo_msg8", CreateDisposition=libsmb.FILE_OPEN);
+        echoes = [b'one', b'two', b'three', b'four']
+        for echo in echoes:
+            c.write(fnum, echo, len(echo), 0)
+            buf = c.read(fnum, 0, 4096)
+            self.assertEqual(buf, echo)
